8d31151fc59a6e2c4eb049349678728714294a24
horiajurcut <horia.dragos@gmail.com>
2019-04-09 20:59:06 +0200

    Conditionals

--- Basics/Main.cpp
+++ Basics/Main.cpp
@@ -3,6 +3,44 @@
 
 int main()
 {
-	Log("Initializing main() function");
+	int a = 8;
+	a++;
+	const char* string = "Hello";
+
+	for (int i = 0; i < 5; i++)
+	{
+		const char c = string[i];
+		std::cout << c << std::endl;
+	}
+
+	int x = 5;
+	bool comparisonResult = x == 5;
+
+	if (comparisonResult)
+	{
+		Log("Hello, world!");
+	}
+
+	const char* ptr = "Yolo!";
+	const char* nptr = nullptr;
+
+	if (ptr)
+	{
+		Log(ptr);
+	}
+	else
+	{
+		Log("Pointer is null!");
+	}
+
+	if (nptr)
+	{
+		Log(nptr)
+	}
+	else
+	{
+		Log("Null Pointer!");
+	}
+
 	std::cin.get();
 }
