cc79071caf12f112651db9c76aa6add54a0e739c
horiajurcut <horia.dragos@gmail.com>
2019-04-09 19:35:37 +0200

    Learn C++ Initial Setup

---
+++ Basics/Logger.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Logger.h"
+
+void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
---
+++ Basics/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Logger.h"
+
+int main()
+{
+	Log("Initializing main() function");
+	std::cin.get();
+}
